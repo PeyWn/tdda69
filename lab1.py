2971c0a7e51bc51f0f8618d20ec80c5a4c50370c
bjohv276 <bjohv276@su15-122.ad.liu.se>
2019-01-22 16:57:24 +0100

    lab1 1.1 to 1.4 complete

--- lab1.py
+++ lab1.py
@@ -25,6 +25,13 @@ def sum_iter(term, lower, successor, upper):
 """
 1.2
 """
+def product(term, lower, succ, upper):
+    if lower > upper:
+        return 1
+    else:
+        return term(lower) *\
+            product(term, succ(lower), succ, upper)
+
 def product_iter(term, lower, successor, upper):
     def iter(lower, result):
         if (lower > upper):
@@ -51,11 +58,35 @@ def wallis(n):
 1.3
 """
 def accumulate(combiner, null, term, lower, succ, upper):
-    pass
+    if lower > upper:
+        return null
+    else:
+        return combiner(term(lower), accumulate(combiner, null, term, succ(lower), succ, upper))
 
 
 def accumulate_iter(combiner, null, term, lower, succ, upper):
-    pass
+    def iter(lower, res):
+        if lower > upper:
+            return res
+        else:
+            return iter(succ(lower), combiner(res, term(lower)))
+    return iter(lower, null)
+
+def acc_sum(lower, upper):
+    return accumulate_iter(lambda x,y:x+y, 0, lambda x:x, lower, lambda x:x+1, upper)
+
+def acc_prod(lower, upper):
+    return accumulate_iter(lambda x,y:x*y, 1, lambda x:x, lower, lambda x:x+1, upper)
+"""
+c)
+Natural numbers
+The combiner can not have diffrent answers in order of execution
+
+Example below:
+print (accumulate_iter(lambda x,y:x-y, 0, lambda x:x, 1, lambda x:x+1, 3), '\n')
+print (accumulate(lambda x,y:x-y, 0, lambda x:x, 1, lambda x:x+1, 3), '\n')
+"""
+
 
 
 """
@@ -66,9 +97,7 @@ def foldl(f, f0, data):
         if not data:
             return f0
         else:
-            last = data[-1]
-            data.remove(data[-1])
-            return f(foldl_iter(data), last)
+            return f(foldl_iter(data[:-1]), data[-1])
     return foldl_iter(data)
 
 
@@ -77,27 +106,25 @@ def foldr(f, f0, data):
         if not data:
             return f0
         else:
-            first = data[0]
-            data.remove(data[0])
-            return f(first, foldl_iter(data))
+            return f(data[0], foldl_iter(data[1:]))
     return foldl_iter(data)
 
 
 def my_map(f, seq):
-    g = lambda m,n: [n, m]
-    return foldr(g, 0, seq)
-
+    return foldl( lambda x,y:x+[f(y)], [], seq )
 
 def reverse_r(seq):
-    pass
+    return foldr( lambda x,y:y+[x], [], seq)
 
-#print(my_map(lambda m,n:m+n, [1,2,3]))
+def reverse_l(seq):
+    return foldl( lambda x,y:[y]+x, [], seq)
 
 
 """
 1.5
 
 F: f(x) --> f^n(x)
+"""
 """
 def repeat(f, n):
     if n == 0:
@@ -109,8 +136,12 @@ def repeat(f, n):
             return iter(n-1, f)
     return iter(n, f)
 
-#print(repeat(lambda n:n*n, 0)(3))
+#print(repeat(lambda n:n*n, 2)(5))
 
+def rep(f):
+    return foldr(lambda seq, g: lambda x: seq(g(x)), lambda x:x, f)
+print(rep(lambda x:x*x)(2))
+"""
 
 def compose(f, g):
     return lambda x: f(g(x))
@@ -125,14 +156,14 @@ def repeated_application():
 """
 1.6
 
-Write a procedure smooth that takes f as input, and returns the smoothed 
+Write a procedure smooth that takes f as input, and returns the smoothed
 version of f with dx=0.01.
 """
 def smooth(f, dx):
     pass
 
 """
-Write a procedure n fold smooth that takes f and n ≥ 0 as inputs and, 
+Write a procedure n fold smooth that takes f and n less or equal 0 as inputs and,
 using your repeat function, returns the n-fold smoothed version of f.
 """
 def n_fold_smooth(f, n):
