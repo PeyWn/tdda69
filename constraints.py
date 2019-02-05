ec836fa020178704835fffc62034976cfa4e4beb
peywn <bjorn.hvass@gmail.com>
2019-02-05 10:23:15 +0100

    Removed trailing whitespaces

--- constraints.py
+++ constraints.py
@@ -8,21 +8,21 @@ class ConnectorError(Exception):
     self.origin = name
     self.old_val = old
     self.new_val = new
-    
+
   def __str__(self):
     return self.origin + ": " + repr(self.old_val) + " is not " + repr(self.new_val)
-    
+
 class Connector:
-  def __init__(self, name_ = "noname"): 
+  def __init__(self, name_ = "noname"):
     self.value  = None
     self.setter = None
     self.name   = name_
     self.constraints = []
     self.show_updates = False
-  
+
   def has_value(self):
     return self.setter is not None
-  
+
   def set_value(self, new_val, new_setter):
     if not self.has_value():
       if self.show_updates:
@@ -32,26 +32,26 @@ class Connector:
       self.__for_each_but(new_setter, lambda constraint: constraint.process(), self.constraints)
     elif not self.value == new_val:
       raise ConnectorError(self.name, self.value, new_val)
-  
-  def forget(self, retractor):    
+
+  def forget(self, retractor):
     if retractor == self.setter:
       if self.show_updates:
         print("FORGET: ", self.name)
       self.setter = None
       self.value = None
-      self.__for_each_but(retractor, lambda constraint: constraint.forget(), self.constraints) 
-      
+      self.__for_each_but(retractor, lambda constraint: constraint.forget(), self.constraints)
+
   def connect(self, new_constraint):
     if new_constraint not in self.constraints:
       self.constraints.append(new_constraint)
     if self.has_value():
       new_constraint.process()
     return "done"
-    
+
   def __for_each_but(self,to_skip, f, seq):
     for e in seq:
       if not e == to_skip: f(e)
-      
+
 class Constant:
   def __init__(self, value, connector):
     self.value = value
@@ -66,17 +66,17 @@ class Adder:
     a1.connect(self)
     a2.connect(self)
     sum.connect(self)
-    
+
   def process(self):
     a1, a2, sum = self.a1, self.a2, self.sum
-    
+
     if a1.has_value() and self.a2.has_value():
       sum.set_value(a1.value + a2.value, self)
     elif a1.has_value() and sum.has_value():
       a2.set_value(self.sum.value - a1.value, self)
     elif a2.has_value() and sum.has_value():
       a1.set_value(sum.value - a2.value, self)
-      
+
   def forget(self):
     self.sum.forget(self)
     self.a1.forget(self)
@@ -91,10 +91,10 @@ class Multiplier:
     m1_.connect(self)
     m2_.connect(self)
     prod_.connect(self)
-  
+
   def process(self):
     m1, m2, prod = self.m1, self.m2, self.prod
-    
+
     if m1.has_value() and m1.value == 0 or \
        m2.has_value() and m2.value == 0:
       prod.set_value(0, self)
@@ -104,10 +104,10 @@ class Multiplier:
       m2.set_value(prod.value / m1.value, self)
       pass
     elif prod.has_value() and m2.has_value():
-      m1.set_value(prod.value / m2.value, self)      
-  
+      m1.set_value(prod.value / m2.value, self)
+
   def forget(self):
-    self.prod.forget(self)    
+    self.prod.forget(self)
     self.m1.forget(self)
     self.m2.forget(self)
     self.process()
