9d28dc614a5b910593be2b29094d02e57924ce32
Jesper Jonsson <jesjo430@student.liu.se>
2019-03-12 15:50:05 +0100

    Probs

--- kl/klib/exception.py
+++ kl/klib/exception.py
@@ -18,8 +18,3 @@ class unknown_native_type(exception):
 class unknown_native_function(exception):
   def __init__(self, type, name):
     super().__init__("Unknown native function {} in {}", name, type)
-
-class catch_exception(Exception):
-  def __init__(self, message, *args, metadata = None):
-    self.message  = message.format(*args)
-    self.metadata = metadata
