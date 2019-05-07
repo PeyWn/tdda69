78d325e4e3ceb90e51c8ffc914ad789a9b7a2077
bjohv276 <bjohv276@su00-106.ad.liu.se>
2019-05-07 10:31:04 +0200

    Lab5: get garbage and use flag done

--- Lab5/Lab5/src/GC/header.py
+++ Lab5/Lab5/src/GC/header.py
@@ -1,16 +1,11 @@
-def header_get_garbage_flag(heap, pointer):
-    print('1', heap)
-    print('2', heap[-1-pointer])
-    print('3', heap[-1-pointer] & 1)
-    print('4', heap[-1-pointer] & 1 == 1)
-    return heap[-1-pointer] & 1 == True
-
+def header_get_garbage_flag(heap, pointer): 
+    return ((heap[pointer+3] & 128) % 127) == 1
 
 def header_set_garbage_flag(heap, pointer, value):
-  pass
+    pass
 
 def header_get_used_flag(heap, pointer):
-  pass
+    return ((heap[pointer+3] & 64) % 63) == 1
 
 def header_set_used_flag(heap, pointer, value):
   pass
@@ -25,7 +20,7 @@ def header_mark_as_bytes_array(heap, pointer):
   pass
 
 def header_get_size(heap, pointer):
-  pass
+    pass
 
 def header_set_size(heap, pointer, size):
   pass
