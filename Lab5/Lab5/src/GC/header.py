5126c118d54ac9c6dc2d49300616ceaa4b7827e7
bjohv276 <bjohv276@su04-101.ad.liu.se>
2019-05-08 10:05:23 +0200

    Lab5: All but set size

--- Lab5/Lab5/src/GC/header.py
+++ Lab5/Lab5/src/GC/header.py
@@ -1,26 +1,41 @@
-def header_get_garbage_flag(heap, pointer): 
-    return ((heap[pointer+3] & 128) % 127) == 1
+def header_get_garbage_flag(heap, pointer):
+    return ((heap[pointer+3] & 128) >> 7) == 1
 
 def header_set_garbage_flag(heap, pointer, value):
-    pass
+    gf = header_get_garbage_flag(heap, pointer)
+    if (value and gf) or (not value and not gf):
+        return heap
+    heap[pointer+3] = (heap[pointer+3] ^ 128)
+    return heap
 
 def header_get_used_flag(heap, pointer):
-    return ((heap[pointer+3] & 64) % 63) == 1
+    return ((heap[pointer+3] & 64) >> 6) == 1
 
 def header_set_used_flag(heap, pointer, value):
-  pass
+    uf = header_get_used_flag(heap, pointer)
+    if (value and uf) or (not value and not uf):
+        return heap
+    heap[pointer+3] = (heap[pointer+3] ^ 64)
+    return heap
 
 def header_is_pointers_array(heap, pointer):
-  pass
+    return ((heap[pointer+3] & 32) >> 5) == 1
+
 
 def header_mark_as_pointers_array(heap, pointer):
-  pass
+    heap[pointer+3] = (heap[pointer+3] | 32)
+    return heap
 
 def header_mark_as_bytes_array(heap, pointer):
-  pass
+    heap[pointer+3] = (heap[pointer+3] & 223)
+    return heap
 
 def header_get_size(heap, pointer):
-    pass
+    header = heap[pointer : pointer+4]
+    header[3] = header[3] & 31
+    MemSize = int.from_bytes(header, byteorder='little')
+    return MemSize
 
 def header_set_size(heap, pointer, size):
-  pass
+    pass
+  
