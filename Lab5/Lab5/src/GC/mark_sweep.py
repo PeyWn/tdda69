3e3ae43a05a823d75981fee6ec03959a3b4d9eff
bjohv276 <bjohv276@su04-113.ad.liu.se>
2019-05-22 15:25:25 +0200

    Lab5: mark sweep done

--- Lab5/Lab5/src/GC/mark_sweep.py
+++ Lab5/Lab5/src/GC/mark_sweep.py
@@ -6,18 +6,30 @@ class mark_sweep(object):
     self.heap = heap
   # This function should collect the memory in the heap
   def collect(self):
-    root_ptr_count = pointer_array_count(self.heap.data, 0)
-    self.heap.data = header_set_garbage_flag(self.heap.data, 0, 1)
-    for i in range(root_ptr_count):
-      root_obj = pointer_array_get(self.heap.data, 0, i)
-      self.heap.data = header_set_garbage_flag(self.heap.data, root_obj, 1)
+
+    self.mark(0)
 
     header_ptr = 0
     while(header_ptr != -1):
       if not header_get_garbage_flag(self.heap.data, header_ptr):
-        self.heap.disallocate(header_ptr)
+        header_ptr = self.heap.disallocate(header_ptr)
 
       header_ptr = self.heap.get_next_header_pointer(header_ptr)
 
   def __rec_collect(self, pointer):
     pass
+
+  def mark(self, root_ptr):
+    if header_get_garbage_flag(self.heap.data, root_ptr):
+        return
+
+    self.heap.data = header_set_garbage_flag(self.heap.data, root_ptr, 1)
+
+    if not header_is_pointers_array(self.heap.data, root_ptr):
+        return
+
+    root_ptr_count = pointer_array_count(self.heap.data, root_ptr)
+    for i in range(root_ptr_count):
+      root_obj = pointer_array_get(self.heap.data, root_ptr, i)
+      print(root_obj)
+      self.mark(root_obj)
