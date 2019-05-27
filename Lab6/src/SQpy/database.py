ad461e83c17ea85b08120a30663e6475e7474830
bjohv276 <bjohv276@su04-113.ad.liu.se>
2019-05-27 17:21:16 +0200

    Lab6: Insert done

--- Lab6/src/SQpy/database.py
+++ Lab6/src/SQpy/database.py
@@ -13,10 +13,27 @@ class database(object):
       return list(f._fields)
 
   def dump_table(self, name):
-      pass
+      res = []
+      for e in self.e_list[name]:
+          res.append(e)
+      return res
+
+
   def execute(self, query):
       if query.token == token.create_table :
           self.t_list[query.name] = namedtuple(query.name, query.columns)
+          self.e_list[query.name] = []
+
       elif query.token == token.insert_into :
           table = self.t_list[query.table]
-          entry = table._make(query.values)
+          table.__new__.__defaults__ = (None,) * len(table._fields)
+
+          try:
+              entry = table._make(query.values)
+          except:
+              entry = table()
+              for i in range(len(query.columns)):
+
+                  entry = entry._replace(**{query.columns[i]:query.values[i]})
+
+          self.e_list[query.table].append(entry)
