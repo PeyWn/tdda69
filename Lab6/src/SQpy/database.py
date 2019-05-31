b09c11cd1ebe835c634c2dedae8f322145b947b5
bjohv276 <bjohv276@su04-104.ad.liu.se>
2019-05-31 11:32:40 +0200

    Lab6: Aggregation draft

--- Lab6/src/SQpy/database.py
+++ Lab6/src/SQpy/database.py
@@ -1,10 +1,12 @@
 from .ast import token, ast
 from collections import namedtuple
+from statistics import mean
 
 class database(object):
   def __init__(self):
       self.t_list = {}
       self.e_list = {}
+      self.from_table = None
   def tables(self):
       return [ e for e in self.t_list.keys()]
 
@@ -33,12 +35,12 @@ class database(object):
           except:
               entry = table()
               for i in range(len(query.columns)):
-
                   entry = entry._replace(**{query.columns[i]:query.values[i]})
 
           self.e_list[query.table].append(entry)
 
       elif query.token == token.delete_from:
+          self.from_table = query.table
           q = self.ast_op(query.where, self.e_list[query.table])
           for e in q:
             self.e_list[query.table].remove(e)
@@ -58,42 +60,57 @@ class database(object):
               self.e_list[query.table][i] = entry
 
       elif query.token == token.select:
+          self.from_table = query.from_table
           if isinstance(query.columns, ast) and query.columns.token == token.star:
               return self.e_list[query.from_table]
 
           else:
 
               fields = []
+              fn_flag = False
               for e in query.columns:
                   if isinstance(e, str):
                       fields.append(e)
                   else:
                       fields.append(e[1])
+                      if e[0].token == (token.fn_count or token.fn_avg):
+                          fn_flag = True
 
               row = namedtuple('row', fields)
 
 
-              res = []
               try:
                   entries = self.ast_op(query.where, self.e_list[query.from_table])
               except:
                   entries = self.e_list[query.from_table]
 
-              for e in entries:
-                  values = []
-                  for f in query.columns:
-                      if isinstance(f, str):
-                          values.append(e._asdict()[f])
-                      else:
-                          values.append(self.ast_op(f[0], [e]))
+              if fn_flag:
+                  res = self.select_by_table(entries, row, query.columns)
+              else:
+                  res = self.select_by_entry(entries, row, query.columns)
 
-                  res.append(row._make(values))
               return res
 
 
+  def select_by_entry(self, entries, row, columns):
+      res = []
+      for e in entries:
+          values = []
+          for f in columns:
+              if isinstance(f, str):
+                  values.append(e._asdict()[f])
+              else:
+                  values.append(self.ast_op(f[0], [e]))
+          res.append(row._make(values))
+      return res
 
-
-
+  def select_by_table(self, table, row, columns):
+      res = []
+      values = []
+      for f in columns:
+          values.append(self.ast_op(f[0], table))
+      res.append(row._make(values))
+      return res
 
   def ast_op(self, node, entry_list):
       if node.token == token.op_and:
@@ -132,7 +149,45 @@ class database(object):
 
       elif node.token == token.op_divide:
           res = []
+          oper = self.eval_operands(node, entry_list)
+          for i in range(len(oper[0])):
+               res.append(oper[0][i]/oper[1][i])
+
+          '''
           for e in entry_list:
               for id in node.operands[0].identifier:
+                  print(id)
                   res = e._asdict()[id]/node.operands[1]
+          '''
+          return res[0]
+
+      elif node.token == token.fn_count:
+          res = 0
+          for e in entry_list:
+              if node.field in e._fields:
+                  res += 1
           return res
+
+      elif node.token == token.fn_avg:
+          return mean([x._asdict()[node.field] for x in entry_list])
+
+  def eval_operands(self, node, entries):
+      l = []
+      for o in node.operands:
+          if isinstance(o, ast):
+
+              if o.token == token.identifier:
+                  if len(o.identifier) == 1:
+                      table = self.from_table
+                      key = o.identifier[0]
+                  else:
+                      table = o.identifier[0]
+                      key = o.identifier[1]
+
+                  print(self.e_list[table])
+                  l.append([e._asdict()[key] for e in self.e_list[table] if e in entries])
+
+          else:
+              l.append([o]*len(entries))
+
+      return l
